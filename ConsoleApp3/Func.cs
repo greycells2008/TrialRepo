2aa3ee24f32bcd7962de316b0b6931ad2bbda0ef
greycells <archanam2008@gmail.com>
2022-04-08 16:39:01 +0530

    Add project files.

---
+++ ConsoleApp3/Func.cs
@@ -0,0 +1,135 @@
+﻿
+
+
+using System;
+using System.IO;
+
+namespace LogFileAssign18Feb
+{
+    internal class Func
+    {
+        internal bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Read Log File");
+            Console.WriteLine("2) Write Log File");
+            Console.WriteLine("3) Exit");
+            Console.Write("\r\nSelect an option: ");
+            string res = Console.ReadLine().Trim();
+
+            try
+            {
+                switch (Convert.ToInt16(res))
+                {
+                    case 1:
+                        this.readFile();
+                        return true;
+                    case 2:
+                        this.writeFile();
+                        return true;
+                    case 3:
+                        return false;
+                    default:
+                        Console.WriteLine("Please enter option(1 or 2 or 3!");
+                        Console.WriteLine("Press any Key!");
+                        Console.ReadKey();
+                        return true;
+                }
+            }
+            catch (InvalidCastException)
+            {
+                Console.WriteLine("Error occured! Please enter 1, 2 or 3 ");
+                Console.WriteLine("Press any Key!");
+                Console.ReadKey();
+                return true;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter option 1, 2 or 3!");
+                Console.WriteLine("Press any Key!");
+                Console.ReadKey();
+                return true;
+
+            }
+
+        }
+        internal void readFile()
+        {
+            char resRF;
+            try
+            {
+                if (File.Exists("LogFile.txt"))
+                {
+                    // Read a text file line by line.  
+                    string[] lines = File.ReadAllLines("LogFile.txt");
+                    foreach (string line in lines)
+                        Console.WriteLine(line);
+                    Console.WriteLine("Press any Key!");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("File doesn't exist!");
+                    Console.WriteLine("Do you want to create log file? (Y or N)");
+                    resRF = Console.ReadKey().KeyChar;
+                    if (resRF == 'Y')
+                    {
+                        this.writeFile();
+                        return;
+                    }
+                    else if (resRF == 'N')
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter Y or N!");
+
+                        Console.ReadKey();
+                    }
+                }
+            }
+            catch (IOException io)
+            {
+                Console.WriteLine("Error occured " + io.Message);
+            }
+
+        }
+
+        internal void writeFile()
+        {
+            try
+            {
+
+                FileInfo f1 = new FileInfo("LogFile.txt");
+
+                StreamWriter sw = f1.AppendText();
+                //Get a StreamWriter for the file
+
+
+                Console.Write("Please enter remarks to be added to log file:");
+
+                //Get the name from the console
+                string remarks = Console.ReadLine();
+
+                //Write to file
+                sw.WriteLine("User Name:" + System.Security.Principal.WindowsIdentity.GetCurrent().Name);
+                sw.WriteLine("Remarks :" + remarks);
+
+                sw.WriteLine("Current Date time :" + DateTime.Now);
+                Console.WriteLine("Information Saved!");
+                Console.WriteLine();
+
+                //Close the writer and file
+                sw.Close();
+                Console.WriteLine("Press any Key!");
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An IO Exception Occurred :" + e);
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace jaggedarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][][] GPSArray = new string[2][][];
+            for(int i=0;i<GPSArray.Length;i++)
+            {
+                Console.Write("Enter name of the place");
+                GPSArray[i][0][0] = Console.ReadLine();
+                Console.Write("Enter longitude of the place");
+                GPSArray[i][1][0] = Console.ReadLine();
+                Console.Write("Enter latitude of the place");
+                GPSArray[i][2][0] = Console.ReadLine();
+                Console.Write("Enter distance of the place");
+                GPSArray[i][3][0] = Console.ReadLine();
+                Console.Write("Enter number of places of interest:");
+                int.TryParse(Console.ReadLine(), out int Numofplaces);
+                string[] placesofinterest = new string[Numofplaces];
+                for(int j=0;j<Numofplaces;j++)
+                {
+                    placesofinterest[j] = Console.ReadLine();
+                }
+                GPSArray[i][4] = placesofinterest;
+            }
+            for (int i = 0; i < GPSArray.Length; i++)
+            {
+                Console.Write(GPSArray[i][0][0]);
+                Console.Write(GPSArray[i][1][0]);
+                Console.Write(GPSArray[i][2][0]);
+                Console.Write(GPSArray[i][3][0]);
+                for(int x=0;x<GPSArray[i][4].Length;x++)
+                {
+                    Console.Write(GPSArray[i][4][x]);
+                }
+            }
+        }
+    }
+}
